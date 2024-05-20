caf444659bfaac7d0c6edf284bfc306104fec98b
ManikantaPotnuru9176 <manikantapotnur9176@gmail.com>
2024-05-20 10:18:25 +0530

    chore: added env

--- src/lib/apollo/apolloClient.ts
+++ src/lib/apollo/apolloClient.ts
@@ -1,9 +1,10 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+import { env } from '@env'
 import fetch from 'node-fetch'
 
 const httpLink = new HttpLink({
-  uri: 'https://hasura-template-production.up.railway.app/v1/graphql',
+  uri: env.HASURA_URI,
   //@ts-ignore
   fetch,
 })
@@ -13,8 +14,8 @@ const authLink = setContext((_, { headers }) => {
     headers: {
       ...headers,
       //   authorization: `Bearer 8ba64bb8-5720-491e-bc46-eea64a9e11ff`,
-      authorization: `Bearer 8ba64bb8-5720-491e-bc46-eea64a9e11ff`,
-      'x-hasura-admin-secret': 'iwillhack',
+      authorization: `Bearer ${env.RAILWAY_SUPER_API}`,
+      'x-hasura-admin-secret': env.HASURA_API_KEY,
     },
   }
 })
