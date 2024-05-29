314feef6834fb1980c8cdc036f8cadce4e9c3d2b
manoj karajada <manojkarajada.mk@gmail.com>
2024-05-29 14:00:24 +0530

    chore : logout in home page layout

--- src/app/(app)/(marketing)/(non-protected)/[[...route]]/layout.tsx
+++ src/app/(app)/(marketing)/(non-protected)/[[...route]]/layout.tsx
@@ -3,7 +3,9 @@ import localFont from 'next/font/local'
 import Link from 'next/link'
 
 // import { SiteFooter } from '@/components/site-footer'
-import { buttonVariants } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { signOut } from '@/lib/auth'
+import { getCurrentUser } from '@/lib/payload'
 import { cn } from '@/utils'
 
 import { MainNav } from './_components/main-nav'
@@ -25,20 +27,42 @@ const fontHeading = localFont({
 export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
+  const user = await getCurrentUser()
+
   return (
     <div className='flex min-h-screen flex-col'>
       <header className='container z-40 bg-background'>
         <div className='flex h-20 items-center justify-between py-6'>
           <MainNav />
           <nav>
-            <Link
-              href='/sign-in'
-              className={cn(
-                buttonVariants({ variant: 'secondary', size: 'sm' }),
-                'px-4',
-              )}>
-              Login
-            </Link>
+            {user ? (
+              <form
+                action={async () => {
+                  'use server'
+                  await signOut()
+                }}>
+                <Button type='submit' variant='destructive'>
+                  Logout
+                </Button>
+              </form>
+            ) : (
+              // <Button
+              //   onClick={() => signOut()}
+              //   className={cn(
+              //     buttonVariants({ variant: 'secondary', size: 'sm' }),
+              //     'px-4',
+              //   )}>
+              //   Logout
+              // </Button>
+              <Link
+                href='/sign-in'
+                className={cn(
+                  buttonVariants({ variant: 'secondary', size: 'sm' }),
+                  'px-4',
+                )}>
+                Login
+              </Link>
+            )}
           </nav>
         </div>
       </header>
