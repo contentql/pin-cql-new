739d8ddf39ad90f11b8ad05a24e8e667b3804013
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-06-05 17:38:57 +0530

    chore: updated stripe manage subscription

--- src/trpc/router/stripe.ts
+++ src/trpc/router/stripe.ts
@@ -1,39 +1,24 @@
 import { env } from '@env'
+import Stripe from 'stripe'
 
 import { router, userProcedure } from '@/trpc'
 
-const stripeSDK = require('stripe')(env.STRIPE_SECRET_KEY)
+const stripeSDK = new Stripe(env.STRIPE_SECRET_KEY)
 
 export const stripe = router({
-  createCustomerPortalSession: userProcedure.mutation(async () => {
-    try {
-      const session = await stripeSDK.billingPortal.sessions.create({
-        customer: 'cus_QEi9xmIlUc6u5d',
-        return_url: 'http://localhost:3000/dashboard',
-      })
+  createCustomerPortalSession: userProcedure.mutation(
+    async ({ ctx: { user } }) => {
+      try {
+        const session = await stripeSDK.billingPortal.sessions.create({
+          customer: user.stripeCID as string,
+          return_url: env.PAYLOAD_URL,
+        })
 
-      return session
-    } catch (error) {
-      console.log(error)
-      throw new Error('Unable to create customer portal session.')
-    }
-  }),
-
-  createCustomer: userProcedure.mutation(async ({ ctx }) => {
-    try {
-      const { user } = ctx
-
-      const customer = await stripeSDK.customers.create({
-        name: user.name,
-        email: user.email,
-      })
-
-      console.log(customer)
-
-      return customer
-    } catch (error) {
-      console.log(error)
-      throw new Error('Unable to create a stripe customer')
-    }
-  }),
+        return session
+      } catch (error) {
+        console.log(error)
+        throw new Error('Unable to create customer portal session.')
+      }
+    },
+  ),
 })
