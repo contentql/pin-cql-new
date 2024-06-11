dc2dcf946350808642a0e6a1e1508ded006d6953
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-06-11 17:59:31 +0530

    chore: testing env variables

--- src/app/my-route/route.ts
+++ src/app/my-route/route.ts
@@ -1,8 +1,11 @@
+import { env } from '@env'
+
 export const GET = async () => {
   return Response.json({
     envPayload: process.env.PAYLOAD_URL,
     envPublic: process.env.NEXT_PUBLIC_PUBLIC_URL,
     systemVariableVercelURL: process.env.VERCEL_URL,
     systemVariableVercelProductionURL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
+    envBasedValue: env.PAYLOAD_URL
   })
 }
