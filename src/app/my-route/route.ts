ff9ce14e05e41c891bc48a72cd6a56ade4c4fd7e
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2024-06-11 17:52:32 +0530

    chore: checking env variables

--- src/app/my-route/route.ts
+++ src/app/my-route/route.ts
@@ -1,14 +1,8 @@
-import configPromise from '../../../payload.config'
-import { getPayload } from 'payload'
-
 export const GET = async () => {
-  const payload = await getPayload({
-    config: configPromise,
+  return Response.json({
+    envPayload: process.env.PAYLOAD_URL,
+    envPublic: process.env.NEXT_PUBLIC_PUBLIC_URL,
+    systemVariableVercelURL: process.env.VERCEL_URL,
+    systemVariableVercelProductionURL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
   })
-
-  const data = await payload.find({
-    collection: 'users',
-  })
-
-  return Response.json(data)
 }
