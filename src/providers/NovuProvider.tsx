0337bc0ac2d2383a766f3e38225aae8850bb811f
ManikantaPotnuru9176 <manikantapotnur9176@gmail.com>
2024-06-12 12:07:27 +0530

    chore: configured stores for multi tabs

--- src/providers/NovuProvider.tsx
+++ src/providers/NovuProvider.tsx
@@ -14,8 +14,25 @@ export const NovuNotificationCenterProvider: React.FC<Props> = ({ user }) => {
   return (
     <NovuProvider
       subscriberId={user.id}
-      applicationIdentifier={env.NEXT_PUBLIC_NOVU_APPLICATION_IDENTIFIER}>
-      <PopoverNotificationCenter colorScheme='light'>
+      applicationIdentifier={env.NEXT_PUBLIC_NOVU_APPLICATION_IDENTIFIER}
+      // i18n={{
+      //   lang: 'de',
+      //   translations: {
+      //     poweredBy: 'ContentQL',
+      //   },
+      // }}
+      stores={[
+        { storeId: 'default_store' },
+        { storeId: 'Deployments', query: { feedIdentifier: 'Deployments' } },
+        { storeId: 'General', query: { feedIdentifier: 'General' } },
+      ]}>
+      <PopoverNotificationCenter
+        colorScheme='light'
+        tabs={[
+          { name: 'All', storeId: 'default_store' },
+          { name: 'General', storeId: 'General' },
+          { name: 'Deployments', storeId: 'Deployments' },
+        ]}>
         {({ unseenCount }) => <NotificationBell unseenCount={unseenCount} />}
       </PopoverNotificationCenter>
     </NovuProvider>
