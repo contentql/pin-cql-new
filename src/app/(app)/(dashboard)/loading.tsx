b96702ef0c1a5b8b413e3deb5701cfbcdb759e70
girish chalumuri <girishchalumuri@gmail.com>
2024-06-13 20:31:23 +0530

    chore : delete loading state

--- src/app/(app)/(dashboard)/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { LoadingSkeleton } from './_components/LoadingSkeleton'
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <LoadingSkeleton />
-}
